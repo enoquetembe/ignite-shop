74b0a4f5addb637b107a7bd9d4b0078e9bfbb2a7
enoquetembe <enoquetembetembe10@gmail.com>
2023-07-26 15:09:40 -0300

    fix: stripe

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe"
 
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY as string
+const stripeSecretKey = 'sk_test_51Mz0wKDz9I4Mp2e8PLYLlAWCzxoFqvNCn3F0sOI0pJHSnNNgeSXJm6M5jpscYYTOhByzUPi9W8jigNQWeXcUCKaT0051dgujdS'
 
 export const stripe = new Stripe(stripeSecretKey, {
   apiVersion: '2022-11-15',
